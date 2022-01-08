d6239e6944d94d97fef5d5a1af6ef91f771b944e
kibuchi-muchai <dickson.kibuchi@student.moringaschool.com>
2022-01-08 11:08:56 +0300

    addedapikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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'
 };
 
 /*
